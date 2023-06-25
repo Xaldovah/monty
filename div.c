edc0c93c042e68a38f30228bb52cd8dd3b8fc509
Dennis Kimani <dxaldovah@gmail.com>
2023-06-25 13:38:57 -0700

    changes to the stack of div and sub

--- div.c
+++ div.c
@@ -9,13 +9,22 @@
  */
 void _div(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack)->next == NULL)
+	stack_t *temp;
+	(void) line_number;
+
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L<line_number>: can't div, stack too short\n");
-		free_info();
-		free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->next->n /= (*stack)->n;
-	pop(stack, line_number);
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L<line_number>: division by zero\n");
+                exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n /= temp->n;
+	(*stack)->prev = NULL;
+	free(temp);
 }
--- sub.c
+++ sub.c
@@ -8,13 +8,18 @@
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack)->next == NULL)
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		free_info();
 		free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->next->n -= (*stack)->n;
-	pop(stack, line_number);
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n -= temp->n;
+	(*stack)->prev = NULL;
+	free(temp);
 }
