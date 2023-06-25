6c3739acc2179b5ef605e39e9505d3ee46220df2
Dennis Kimani <dxaldovah@gmail.com>
2023-06-25 03:29:16 -0700

    changes to the implimentation of the code

--- sub.c
+++ sub.c
@@ -8,18 +8,13 @@
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
-
-	(void) line_number;
-
 	if (stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L<line_number>: can't sub, stack too short\n");
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		free_info();
+		free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	temp = *stack;
-	*stack = (*stack)->next;
-	(*stack)->n -= temp->n;
-	(*stack)->prev = NULL;
-	free(temp);
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
 }
